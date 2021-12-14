604ed5155f76e77c269a7292d700256831ba4780
Alex <alejandro.rod.lop@gmail.com>
2021-12-14 20:41:57 +0100

    Added Sesion 9 Files

--- Sesion9/src/app/App.java
+++ Sesion9/src/app/App.java
@@ -9,11 +9,11 @@ public class App {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Creating Hash...");
-			LPCHash <Integer> H1 = new LPCHash<Integer> ();
+			LPCHash<Integer> H1 = new LPCHash<Integer>();
 			System.out.println("Creating custom-sized Hash...");
-			LPCHash <Integer> H2 = new LPCHash<Integer> (10);
+			LPCHash<Integer> H2 = new LPCHash<Integer>(10);
 			System.out.println("Creating fully-custom Hash...");
-			LPCHash <Integer> H3 = new LPCHash<Integer> (10, 0.75);
+			LPCHash<Integer> H3 = new LPCHash<Integer>(10, 0.75);
 			System.out.println("Adding elements to Hash1, to force resize...");
 			H1.add(7);
 			H1.add(6);
@@ -23,22 +23,28 @@ public class App {
 			H1.add(53);
 			H1.add(64);
 			H1.add(69);
-			System.out.println("Hash1");
+			System.out.println("H1");
 			System.out.println(H1.toString());
-			LPCHash <Integer> H4 = new LPCHash<Integer> (H1);
+			System.out.println("Creating copy Hash from H1...");
+			LPCHash<Integer> H4 = new LPCHash<Integer>(H1);
+			System.out.println("H4");
 			System.out.println(H4.toString());
-			Iterator <Integer> itr = H4.iterator();
+			System.out.println("Removing first element returned by next() from H4...");
+			Iterator<Integer> itr = H4.iterator();
 			itr.next();
 			itr.remove();
+			System.out.println("H4");
 			System.out.println(H4.toString());
+			System.out.println("Adding all elements from H1 to H2");
 			H2.addAll(H1);
+			System.out.println("Removing 7 from H2");
 			H2.remove(7);
+			System.out.println("H2");
 			System.out.println(H2.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		
 	}
 
 }
--- Sesion9/src/estDatos/LPCHash.java
+++ Sesion9/src/estDatos/LPCHash.java
@@ -3,34 +3,35 @@ package estDatos;
 import java.util.AbstractSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.Collection;
 
-public class LPCHash<E> extends AbstractSet<E> {
+public class LPCHash <E> extends AbstractSet<E> implements Set<E> {
 
 	private static final double DEFAULT_LOAD_FACTOR_LIMIT = 0.5;
 	private static final int DEFAULT_CAPACITY = 11;
-
-	private E[] table; // tabla de elementos
-	private int[] status; // estados->0:vacío;1:borrado;2:ocupado
-	private int tablesize; // tamaño de la tabla
-	private double loadFactorLimit; // factor de carga límite
-	private int elements; // número de elementos
-	private int deleted; // número de posiciones borradas
-
-	public LPCHash() throws IllegalArgumentException {
+	
+	private	E[]	table;					 //tabla	de	elementos
+	private	int[]	status;				 //estados->0:vacío;1:borrado;2:ocupado
+	private	int	tablesize;				 //tamaño	de	la	tabla
+	private	double	loadFactorLimit;	 //factor	de	carga	límite
+	private	int	elements;				 //número	de	elementos
+	private	int	deleted;				 //número	de	posiciones	borradas
+	
+	public LPCHash () throws IllegalArgumentException {
 		this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR_LIMIT);
 	}
-
-	public LPCHash(int initialCapacity) throws IllegalArgumentException {
+	
+	public LPCHash (int initialCapacity) throws IllegalArgumentException {
 		this(initialCapacity, DEFAULT_LOAD_FACTOR_LIMIT);
 	}
-
+	
 	@SuppressWarnings("unchecked")
-	public LPCHash(int initialCapacity, double theLoadFactor) throws IllegalArgumentException {
+	public LPCHash (int initialCapacity, double theLoadFactor) throws IllegalArgumentException {
 		if (theLoadFactor > 1 || theLoadFactor < 0 || initialCapacity <= 0) {
 			throw new IllegalArgumentException();
 		}
-		this.table = (E[]) new Object[initialCapacity];
+		this.table = (E[]) new Object [initialCapacity];
 		this.elements = 0;
 		this.tablesize = initialCapacity;
 		this.loadFactorLimit = theLoadFactor;
@@ -40,35 +41,34 @@ public class LPCHash<E> extends AbstractSet<E> {
 			this.status[i] = 0;
 		}
 	}
-
-	public LPCHash(Collection<? extends E> c) throws IllegalArgumentException {
+	
+	public LPCHash (Collection<? extends E> c) throws IllegalArgumentException {
 		this();
 		this.addAll(c);
 	}
-
+	
 	/**
 	 * Returns the hash value of the element e
 	 * 
 	 * @param e Element to be calculated
 	 * @return Hash of e
 	 */
-	private int hash(E e) {
+	private int hash (E e) {
 		return e.hashCode() % this.tablesize;
 	}
-
+	
 	/**
 	 * Returns the hash value of e after certain collisions
-	 * 
-	 * @param e   Element to be calculated
+	 * @param e Element to be calculated
 	 * @param col Number of collisions
 	 * @return Hash of e
 	 */
-	private int rehash(E e, int col) {
-		return (hash(e) + col) % this.tablesize;
+	private int rehash (E e, int col) {
+		return (hash(e)+col) % this.tablesize;
 	}
-
+	
 	@Override
-	public boolean add(E e) {
+	public boolean add (E e) {
 		if (e == null) {
 			throw new NullPointerException();
 		}
@@ -87,26 +87,25 @@ public class LPCHash<E> extends AbstractSet<E> {
 		this.elements++;
 		return true;
 	}
-
+	
 	/**
-	 * Checks if the number of values int the Structure over the size of the
-	 * Structure is higher than the load factor limit
+	 * Checks if the number of values int the Structure over the size of the Structure is higher than the load factor limit
 	 *
 	 * @return True if there is Overload
 	 */
 	private boolean checkOverLoadFactor() {
-		return (((double) this.size()) / this.tablesize) > this.loadFactorLimit;
+		return (((double)this.size()) / this.tablesize) > this.loadFactorLimit;
 	}
-
+	
 	/**
 	 * Resizes the structure given a multiplier
 	 * 
 	 * @param multiplier Multipliier by which the structure size will be increased
 	 */
-	public void resize(int multiplier) {
+	public void resize (int multiplier) {
 		this.tablesize = multiplier * this.tablesize + 1;
 		try {
-			LPCHash<E> neu = new LPCHash<E>(this.tablesize, this.loadFactorLimit);
+			LPCHash <E> neu = new LPCHash <E> (this.tablesize, this.loadFactorLimit);
 			neu.addAll(this);
 			this.status = neu.status;
 			this.table = neu.table;
@@ -114,14 +113,14 @@ public class LPCHash<E> extends AbstractSet<E> {
 			e1.printStackTrace();
 		}
 	}
-
+	
 	/**
 	 * Returns the first position of the structure that is empty or equal to e
 	 * 
 	 * @param e Element to be searched for
 	 * @return Position in the structure
 	 */
-	private int firstEqualEmpty(E e) {
+	private int firstEqualEmpty (E e) {
 		if (e == null) {
 			throw new NullPointerException();
 		}
@@ -132,14 +131,14 @@ public class LPCHash<E> extends AbstractSet<E> {
 		}
 		return pos;
 	}
-
+	
 	/**
 	 * Returns the first position of the structure that has been erased or is empty
 	 * 
 	 * @param e Element to be searched with
 	 * @return Position in the structure
 	 */
-	private int firstEraseEmpty(E e) {
+	private int firstEraseEmpty (E e) {
 		if (e == null) {
 			throw new NullPointerException();
 		}
@@ -150,47 +149,47 @@ public class LPCHash<E> extends AbstractSet<E> {
 		}
 		return pos;
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public boolean contains(Object e) {
+	public boolean contains (Object e) {
 		if (e == null) {
 			throw new NullPointerException();
 		}
 		return this.status[firstEqualEmpty((E) e)] == 2;
 	}
-
+	
 	@SuppressWarnings("unchecked")
-	public boolean remove(Object e) {
+	public boolean remove (Object e) {
 		if (e == null) {
 			throw new NullPointerException();
 		}
 		if (!this.contains(e)) {
 			return false;
 		}
-		this.status[firstEqualEmpty((E) e)] = 1;
+		this.status[firstEqualEmpty((E)e)] = 1;
 		this.deleted++;
 		this.elements--;
 		return true;
 	}
-
+	
 	@Override
 	public Iterator<E> iterator() {
-		return new LPCHI();
+		return new LPCHI ();
 	}
 
-	private final class LPCHI implements Iterator<E> {
+	private final class LPCHI implements Iterator <E> {
 
 		private int idx;
 		private int visited;
 		private E lastVisited;
-
-		public LPCHI() {
+		
+		public LPCHI () {
 			this.visited = 0;
 			this.idx = -1;
 			findNext();
 		}
-
+		
 		@Override
 		public boolean hasNext() {
 			return visited < elements && idx < tablesize;
@@ -204,23 +203,23 @@ public class LPCHash<E> extends AbstractSet<E> {
 			findNext();
 			return e;
 		}
-
+		
 		/**
 		 * Finds the next position that has an element
 		 * 
 		 * @return Next position with element
 		 */
 		private int findNext() {
-			while (hasNext() && status[++idx] != 2) {
-
+			while(hasNext() && status[++idx] != 2) {
+				
 			}
 			return idx;
 		}
-
+		
 		/**
 		 * Removes the last visited element
 		 */
-		public void remove() {
+		public void remove () {
 			if (this.lastVisited == null) {
 				throw new NoSuchElementException();
 			}
@@ -229,14 +228,14 @@ public class LPCHash<E> extends AbstractSet<E> {
 			this.lastVisited = null;
 		}
 	}
-
+	
 	@Override
 	public int size() {
 		return this.elements + this.deleted;
 	}
-
+	
 	@Override
-	public String toString() {
+	public String toString () {
 		String string = "Table\n";
 		for (E e : this) {
 			string = string + "\t\t" + e.toString() + "\n";
